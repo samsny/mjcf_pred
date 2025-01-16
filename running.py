2e6bfdc4ea4f949018e1ca14e8c0cd79b44c3886
samsny <71847126+samsny@users.noreply.github.com>
2025-01-16 14:29:31 -0500

    Update and rename walking.py to running.py

--- running.py
+++ running.py
@@ -20,16 +20,16 @@ import os
 
 print(tf.__version__)
 
-#### Load in data, remove Nan, reshape and scale from range 0 to 1
+# Load in data & reshape and scale from range 0 to 1
 path = "C:/Running_Data/"
 df_Y = pd.DataFrame()
 df_X = pd.DataFrame()
 for filename in os.listdir(path):
-    if filename.endswith("R_Fmedial.csv"):
-        data = pd.read_csv(filename,header = None).fillna(0)
+    if filename.endswith("R_Fmedial.csv"): % Ground Truth Data
+        data = pd.read_csv(filename,header = None)
         df_Y = pd.concat([df_Y,data],axis = 1)
-    elif filename.endswith("R_Sensor.csv"):
-        data = pd.read_csv(filename,header = None).fillna(0)
+    elif filename.endswith("R_Sensor.csv"): % Sensor Data
+        data = pd.read_csv(filename,header = None)
         df_X = pd.concat([df_X,data],axis = 1)
     else: 
         continue
@@ -51,23 +51,25 @@ np_x = np.moveaxis(np_X.reshape(samples,101,-1,1),1,1)
 np_y = np_Y.transpose(-1,0)
 print(np_x.shape,np_y.shape)
 
-##### Set groups of each subject and build model
+# Set groups of each subject 
 groups = np.concatenate([np.ones(12)*1, np.ones(11)*2, np.ones(14)*3, np.ones(17)*4,
                          np.ones(7)*5, np.ones(11)*6, np.ones(16)*7, np.ones(17)*8])
-len(groups)
 
-# Build model 
+#Build model 
 model = keras.Sequential()
-model.add(layers.Conv2D(16, (15, 1), activation = 'relu', input_shape = (101,15,1)))
-# model.add(layers.MaxPooling2D(1,2))
-model.add(layers.Conv2D(64, (15,1), activation = 'relu'))
-# model.add(layers.MaxPooling2D(1,2))
+model.add(layers.Conv2D(128, (15, 1), activation = 'relu', input_shape = (101,15,1)))
+model.add(layers.Dropout(rate = 0.1))
 model.add(layers.Conv2D(48, (15,1), activation = 'relu'))
+model.add(layers.Dropout(rate = 0.1))
+model.add(layers.Conv2D(176, (15,1), activation = 'relu'))
+model.add(layers.Dropout(rate = 0.1))
 model.add(layers.Flatten())
 model.add(layers.Dense(101,activation = 'relu'))
-model.add(layers.Dropout(rate = 0.1))
+model.add(layers.Dropout(rate = 0))
 model.add(layers.Dense(101))
-model.add(layers.Dropout(rate = 0.1))
+model.add(layers.Dropout(rate = 0))
+
+optimizer = keras.optimizers.Adam(learning_rate=1e-3)
 
 # Compile model
 model.compile(
@@ -91,8 +93,18 @@ for train, test in logo.split(np_x, np_y, groups = groups):
     print("xtrain:",x_train.shape, "xtest:",x_test.shape, 
       "ytrain:",y_train.shape,"ytest:",y_test.shape)
 
-    mod = model.fit(x_train, y_train, batch_size = 23, epochs = 100) # batch size should be number of samples
    
+    mod = model.fit(x_train, y_train,  epochs = 100,
+                    callbacks = [tf.keras.callbacks.EarlyStopping(monitor = 'loss',patience=15)], batch_size = 23, verbose= 0) # batch size should be number of samples
+   # summarize history for loss
+    plt.plot(mod.history['loss'])
+    plt.plot(mod.history['mse']) #RAISE ERROR
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    
     scores = model.evaluate(x_test, y_test) # calculate mse score
     mse.extend(scores) # store mse
     
@@ -102,23 +114,7 @@ for train, test in logo.split(np_x, np_y, groups = groups):
     expected = y_test # expected result of prediction
     val.extend(expected)
 
-#### Calculate mean and standard deviation of results
-# Calculate mean and standard deviation of mse from each epoch
+# Calculate mean and standard deviation of mse for results
 avg_mse = np.mean(mse)
 std_mse = np.std(mse)
 print("Mean mse:", avg_mse, "Std:", std_mse)
-
-# Calculate mean and standard deviation of predictions (acutal results)
-result = np.array(result)
-avg_p = np.mean(result, axis=0)
-avg_p = avg_p.flatten()
-std_p = np.std(result, axis=0)
-std_p = std_p.flatten()
-
-# Calculate mean and standard deviation of test data (expected results)
-val = np.array(val)
-avg_t = np.mean(val, axis = 0)
-avg_t = avg_t.flatten()
-std_t = np.std(val, axis =0)
-std_t = std_t.flatten()
-print("Test Data STD:", np.mean(std_t))
